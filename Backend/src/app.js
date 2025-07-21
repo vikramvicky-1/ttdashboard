7c0b6ecf51fff94124e7365587e3a9fe865314f7
Vikram Vicky <123199376+vikramvicky-1@users.noreply.github.com>
2025-07-21 14:47:14 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // ✅ Updated CORS setup
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000","https://ttdashboard.onrender.com"],
   })
 );
 
