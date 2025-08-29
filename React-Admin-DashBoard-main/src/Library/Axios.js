79a3fca8a4f9afac5458e3bb2e239e71729314ed
vikram <123199376+vikramvicky-1@users.noreply.github.com>
2025-08-29 20:03:21 +0530

    hosting done

--- React-Admin-DashBoard-main/src/Library/Axios.js
+++ React-Admin-DashBoard-main/src/Library/Axios.js
@@ -4,7 +4,6 @@ const axiosInstance = axios.create({
   // Development URL - change to production URL when deploying
   // baseURL: "http://localhost:5000/api",
   baseURL: "https://ttdashboard04.onrender.com/api",
-  // Production URL: "https://ttdashboard04.onrender.com/api"
 });
 
 // Add request interceptor to include auth token
